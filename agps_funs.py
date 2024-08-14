17730155a0cbe6581cff4c9d5e4fa03b55bd0217
m-waltert <wate@zhaw.ch>
2024-08-14 16:07:30 +0200

    Development and testing of alternative pushback detection method

--- agps_funs.py
+++ agps_funs.py
@@ -4,7 +4,11 @@ import numpy as np
 from datetime import timedelta
 
 
-def takeoff_taxi_detection(traj, rwy_geometries, df_rwys, airport_str, maxHoleDuration=60):
+def takeoff_taxi_detection(traj, 
+                           rwy_geometries, 
+                           df_rwys, 
+                           airport_str,
+                           maxHoleDuration=60):
     """
     Detects takeoff and taxiing events from trajectory data, identifies the takeoff runway, and computes the taxi duration.
 
@@ -59,8 +63,8 @@ def takeoff_taxi_detection(traj, rwy_geometries, df_rwys, airport_str, maxHoleDu
                     continue
 
 
-                if clipped_traj is None or clipped_traj.data.empty:
-                    continue
+                # if clipped_traj is None or clipped_traj.data.empty:
+                #     continue
 
                 # Check whether there is a "hole" in the traj, which can happen for runway 16 departures, that cross runway 10/28 (and the software
                 # then wrongfully assumes that it is runway 10/28 departure)
@@ -113,6 +117,7 @@ def takeoff_taxi_detection(traj, rwy_geometries, df_rwys, airport_str, maxHoleDu
             if (startTaxi is not np.nan) and (lineupTime is not np.nan):
                 taxiDuration = lineupTime - startTaxi
 
+
     traj.data.loc[:, 'isPushback'] = isPushback
     traj.data.loc[:, 'startPushback'] = startPushback
     traj.data.loc[:, 'startTaxi'] = startTaxi
@@ -120,5 +125,69 @@ def takeoff_taxi_detection(traj, rwy_geometries, df_rwys, airport_str, maxHoleDu
     traj.data.loc[:, 'lineupTime'] = lineupTime
     traj.data.loc[:, 'taxiDuration'] = taxiDuration
     traj.data.loc[:, 'takeoffRunway'] = takeoffRunway
+
+    return traj
+
+
+def alternative_pushback_detection(traj, standAreas):
+    """ Alternative means of detecting a pushback. Only valid for trajs which show negativ taxi duration."""
+
+    taxiDuration = traj.data.taxiDuration.iloc[0]
+    startPushback = traj.data.startPushback.iloc[0]
+    startTaxi = traj.data.startTaxi.iloc[0]
+    lineupTime = traj.data.lineupTime.iloc[0]
+    isPushback = False
+
+    if taxiDuration < timedelta(seconds=0):
+        for standArea in standAreas:
+            clipped_traj = traj.clip(standArea)
+
+            # Check whether traj is inside stand_area
+            if clipped_traj is None or clipped_traj.data.empty:
+                        continue
+
+
+            df = traj.data
+            nonzeroGS = df.compute_gs.rolling(5).median() > 1
+
+
+            leaveStandTime = clipped_traj.stop
+            leaveStandTimeIndex = df[df['timestamp']==leaveStandTime].index[0]
+
+            # Find the start of the pushback
+            startPushbackIndex = leaveStandTimeIndex
+            while startPushbackIndex > 0 and nonzeroGS.loc[startPushbackIndex - 1]:
+                startPushbackIndex -= 1
+
+            # Find the end of the pushback
+            endPushbackIndex = leaveStandTimeIndex
+            while endPushbackIndex < df.index.max() and nonzeroGS.loc[endPushbackIndex + 1]:
+                endPushbackIndex += 1
+
+            startPushback = df.loc[startPushbackIndex, 'timestamp']
+            startTaxi = df.loc[endPushbackIndex, 'timestamp']
+
+            isPushback = True
+
+            # If start and end of pushback are identical
+            if startPushback == startTaxi:
+                startPushback = startPushback - timedelta(seconds=2)
+
+            # Recalculate taxiDuration
+            if (startTaxi is not np.nan) and (lineupTime is not np.nan):
+                taxiDuration = lineupTime - startTaxi
+
+
+        # If it is not a pushback, assume that taxi starts at begin of traj
+        if not isPushback:
+            startPushback = traj.start
+            startTaxi = traj.start
+            taxiDuration = lineupTime - startTaxi
+        
+    # Write date to traj
+    traj.data.loc[:, 'isPushback'] = isPushback
+    traj.data.loc[:, 'startPushback'] = startPushback
+    traj.data.loc[:, 'startTaxi'] = startTaxi
+    traj.data.loc[:, 'taxiDuration'] = taxiDuration
 
     return traj
