117b4ce072f1658c79ecd14b3772cbd7feb6a9ea
m-waltert <wate@zhaw.ch>
2024-08-15 16:41:33 +0200

    Updated pushback method

--- agps_funs.py
+++ agps_funs.py
@@ -4,11 +4,11 @@ import numpy as np
 from datetime import timedelta
 
 
-def takeoff_taxi_detection(traj, 
-                           rwy_geometries, 
-                           df_rwys, 
-                           airport_str,
-                           maxHoleDuration=60):
+def takeoff_detection(traj, 
+                      rwy_geometries, 
+                      df_rwys, 
+                      airport_str='LSZH',
+                      maxHoleDuration=60):
     """
     Detects takeoff and taxiing events from trajectory data, identifies the takeoff runway, and computes the taxi duration.
 
@@ -43,13 +43,8 @@ def takeoff_taxi_detection(traj,
     """
 
     takeoffRunway = ''
-    taxiDuration = np.nan
     lineupTime = np.nan
-    pushbackDuration = np.nan
-    startPushback = np.nan
-    startTaxi = np.nan
     isTakeoff = False
-    isPushback = False
 
     if traj.takeoff_from(airport_str):
 
@@ -59,12 +54,8 @@ def takeoff_taxi_detection(traj,
                 # Clip traj to runway geometry
                 clipped_traj = traj.clip(rwy)
 
-                if clipped_traj is None or clipped_traj.data.empty:
+                if (clipped_traj is None) or (clipped_traj.data.empty): #or (clipped_traj.duration < timedelta(seconds=60)):
                     continue
-
-
-                # if clipped_traj is None or clipped_traj.data.empty:
-                #     continue
 
                 # Check whether there is a "hole" in the traj, which can happen for runway 16 departures, that cross runway 10/28 (and the software
                 # then wrongfully assumes that it is runway 10/28 departure)
@@ -76,7 +67,7 @@ def takeoff_taxi_detection(traj,
                 first_5sec = clipped_traj.first(seconds=5).data
                 last_5sec = clipped_traj.last(seconds=5).data
                 last_20sec = clipped_traj.last(seconds=20).data
-                last_60min_data = traj.last(minutes=60)
+                #last_60min_data = traj.last(minutes=60)
 
                 # Calculate ground speed and vertical rate
                 median_gs = first_5sec.compute_gs.median() if not first_5sec.compute_gs.isna().all() else np.nan
@@ -98,59 +89,62 @@ def takeoff_taxi_detection(traj,
 
                     break
 
-        if isTakeoff:
-            # Check if Pushback executed?
-            pushback = last_60min_data.pushback(airport_str)
-            if pushback is not None:
-                isPushback = True
-                startPushback = pushback.start
-                startTaxi = pushback.stop
-                pushbackDuration = startTaxi - startPushback
-            else:
-                # Check if parking position exists & aircraft is longer on position than 30 seconds
-                parkingPosition = last_60min_data.on_parking_position(airport_str).max()
-                if (parkingPosition is not None) and (parkingPosition.duration > timedelta(seconds=30)):
-                    startTaxi = parkingPosition.stop
-                else:
-                    startTaxi = last_60min_data.start if not last_60min_data.data.empty else np.nan
+        # if isTakeoff:
+        #     # Check if Pushback executed?
+        #     pushback = last_60min_data.pushback(airport_str)
+        #     if pushback is not None:
+        #         isPushback = True
+        #         startPushback = pushback.start
+        #         startTaxi = pushback.stop
+        #         pushbackDuration = startTaxi - startPushback
+        #     else:
+        #         # Check if parking position exists & aircraft is longer on position than 30 seconds
+        #         parkingPosition = last_60min_data.on_parking_position(airport_str).max()
+        #         if (parkingPosition is not None) and (parkingPosition.duration > timedelta(seconds=30)):
+        #             startTaxi = parkingPosition.stop
+        #         else:
+        #             startTaxi = last_60min_data.start if not last_60min_data.data.empty else np.nan
 
-            if (startTaxi is not np.nan) and (lineupTime is not np.nan):
-                taxiDuration = lineupTime - startTaxi
+        #     if (startTaxi is not np.nan) and (lineupTime is not np.nan):
+        #         taxiDuration = lineupTime - startTaxi
 
 
-    traj.data.loc[:, 'isPushback'] = isPushback
-    traj.data.loc[:, 'startPushback'] = startPushback
-    traj.data.loc[:, 'startTaxi'] = startTaxi
-    traj.data.loc[:, 'pushbackDuration'] = pushbackDuration
     traj.data.loc[:, 'lineupTime'] = lineupTime
-    traj.data.loc[:, 'taxiDuration'] = taxiDuration
     traj.data.loc[:, 'takeoffRunway'] = takeoffRunway
+    traj.data.loc[:, 'isTakeoff'] = isTakeoff
 
     return traj
 
 
-def alternative_pushback_detection(traj, standAreas):
+def alternative_pushback_detection(traj, standAreas, airport_str='LSZH'):
     """ Alternative means of detecting a pushback. Only valid for trajs which show negativ taxi duration."""
 
-    taxiDuration = traj.data.taxiDuration.iloc[0]
-    startPushback = traj.data.startPushback.iloc[0]
-    startTaxi = traj.data.startTaxi.iloc[0]
     lineupTime = traj.data.lineupTime.iloc[0]
+    taxiDuration = np.nan
+    startPushback = np.nan
+    pushbackDuration = np.nan
+    startTaxi = np.nan
     isPushback = False
 
-    if taxiDuration < timedelta(seconds=0):
-        for standArea in standAreas:
+    # Apply pushback and taxi detection only to takeoffs
+    if traj.data.isTakeoff.iloc[0]:
+
+
+        # Pushback Detection
+        for i, standArea in enumerate(standAreas):
             clipped_traj = traj.clip(standArea)
 
             # Check whether traj is inside stand_area
-            if clipped_traj is None or clipped_traj.data.empty:
+            if (clipped_traj is None) or (clipped_traj.data.empty):
                         continue
-
+            else:
+                isPushback = True
+                break   
+        
+        if isPushback and (clipped_traj is not None):
 
             df = traj.data
             nonzeroGS = df.compute_gs.rolling(5).median() > 1
-
-
             leaveStandTime = clipped_traj.stop
             leaveStandTimeIndex = df[df['timestamp']==leaveStandTime].index[0]
 
@@ -167,27 +161,54 @@ def alternative_pushback_detection(traj, standAreas):
             startPushback = df.loc[startPushbackIndex, 'timestamp']
             startTaxi = df.loc[endPushbackIndex, 'timestamp']
 
-            isPushback = True
-
             # If start and end of pushback are identical
             if startPushback == startTaxi:
                 startPushback = startPushback - timedelta(seconds=2)
 
-            # Recalculate taxiDuration
-            if (startTaxi is not np.nan) and (lineupTime is not np.nan):
-                taxiDuration = lineupTime - startTaxi
-
-
-        # If it is not a pushback, assume that taxi starts at begin of traj
-        if not isPushback:
-            startPushback = traj.start
-            startTaxi = traj.start
+            # Taxi duration
             taxiDuration = lineupTime - startTaxi
+
+            # Pushback duration
+            pushbackDuration = startTaxi - startPushback
+
+            # Check if taxiDuration is less than 0 seconds. This can happen if the ground coverage is not perfect and parts of the traj are missing.
+            if taxiDuration < timedelta(seconds=0):
+                startTaxi = leaveStandTime
+                isPushback = False
+                startPushback = np.nan
+                pushbackDuration = np.nan
+
+
+            # # Check if taxiDuration is less than 0 seconds. This can happen if the ground coverage is not perfect and parts of the traj are missing.
+            # if taxiDuration < timedelta(seconds=0):
+            #     diffGS = df.compute_gs.diff()
+            #     startTaxiIndex = leaveStandTimeIndex
+            #     while startTaxiIndex < df.index.max() and diffGS.loc[startTaxiIndex + 1]:
+            #         startTaxiIndex += 1
+
+            #     startTaxi = df.loc[startTaxiIndex, 'timestamp']
+            #     taxiDuration = lineupTime - startTaxi
+            #     startPushback = startTaxi - timedelta(seconds=2)
+
+        # If takeoff is not pushback, check for stand
+        else:
+
+            parkingPosition = traj.on_parking_position(airport_str).max()
+
+            if (parkingPosition is not None) and (parkingPosition.duration > timedelta(seconds=30)):
+                startTaxi = parkingPosition.stop
+            # It it is a takeoff, but neither a pushback nor a stand can be detected
+            else:
+                startTaxi = traj.start if not traj.data.empty else np.nan
+
+        # Calculate taxiDuration
+        taxiDuration = lineupTime - startTaxi
         
     # Write date to traj
     traj.data.loc[:, 'isPushback'] = isPushback
     traj.data.loc[:, 'startPushback'] = startPushback
     traj.data.loc[:, 'startTaxi'] = startTaxi
+    traj.data.loc[:, 'pushbackDuration'] = pushbackDuration
     traj.data.loc[:, 'taxiDuration'] = taxiDuration
 
     return traj
